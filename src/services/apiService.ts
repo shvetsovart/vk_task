aebaaf31fbd9ede36d63a7f15cb83b0f9a1f60b0
Артём Швецов <shvetsovart55@gmail.com>
2024-10-30 00:05:37 +0300

    last fixes: app name and site to parse elements

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,7 @@
 import { Repository } from "../models/Repository";
 
 export const fetchRepositories = async (page: number): Promise<{ items: Repository[] }> => {
-    const response = await fetch(`https://api.github.com/search/repositories?q=javascript&sort=stars&order=asc&page=${page}`);
+    const response = await fetch(`https://api.github.com/search/repositories?q=clojure&sort=stars&order=asc&page=${page}`);
     if (!response.ok) throw new Error("Ошибка при загрузке данных");
     return response.json();
 };
